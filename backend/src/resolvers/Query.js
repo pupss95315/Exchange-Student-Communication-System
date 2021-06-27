27a68af9593db12d8512ed5016d6f9e5808b735f
PJL-Emily <b07705053@ntu.edu.tw>
2021-06-27 16:07:23 +0800

    backend update: async query

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,10 +1,12 @@
 const Query = {
-  users(parent, args, { db }, info) {
+  async users(parent, args, { db }, info) {
     if (args.UID) {
-      return [db.users.findOne({ user_id : args.UID })]
+      const ret = [await db.users.findOne({ user_id : args.UID })];
+      return ret;
     }
     if (args.group) {
-      return [db.users.findOne({ group : args.group })]
+      const ret = [await db.users.findOne({ group : args.group })];
+      return ret;
     }
     // if no filter, return all users
     return db.users;  
@@ -12,30 +14,27 @@ const Query = {
   async comments(parent, args, { db }, info) {
     var comments = db.comments;
     if (args.group) {
-      comments = db.comments.find({ group: args.group });
+      comments = await db.comments.find({ group: args.group });
     } else { // if group is not specified, return comments in the main chat
-      comments = db.comments.find({ group: null });
+      comments = await db.comments.find({ group: null });
     }
     // return comments posted by a specified user
     if (args.type === 'SELF') {
-      return comments.find({ author: args.data });
+      const ret = await comments.find({ author: args.data });
+      return ret;
     }
     // return comments containing specified keyword
     if (args.type === 'SEARCH') {
       const prefix = ".*";
-      return comments.find({ content: { $regex: prefix.concat("", args.data).concat("", prefix) } });
+      const ret = await comments.find({ content: { $regex: prefix.concat("", args.data).concat("", prefix) } });
+      return ret;
     }
     // return comments followed by a specified user
     if (args.type === 'FOLLOW') {  
-      return comments.find({ followers: args.data });
+      const ret = await comments.find({ followers: args.data });
+      return ret;
     }
     return comments;
-  },
-  replies(parent, args, { db }, info) {
-    // return all replies under a specified comment
-    return db.replies.filter((reply) => {
-      return reply.comment === args.cmtId;
-    });
   },
 };
 
