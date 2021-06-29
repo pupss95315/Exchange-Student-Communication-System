20449c56dcb27ae0fdc2f74bfebe70a39e8bfedc
PJL-Emily <b07705053@ntu.edu.tw>
2021-06-29 20:46:29 +0800

    frontend update: Sort

--- frontend/src/components/Bulletin.js
+++ frontend/src/components/Bulletin.js
@@ -20,44 +20,28 @@ const Bulletin = ({ UID, setShow, setMsg, group, type, search }) => {
     //console.log("queryData: ", queryData)
     const { loading, error, data, subscribeToMore } = useQuery(COMMENT_QUERY, { variables: { group: group, type: type, data: queryData } });
     const [sort, setSort] = useState("最新");
-    const handleSort = (attribute) => {
-      console.log(attribute)
-      // if(data){
-      //   switch(attribute){
-      //     case "datetime":
-      //       console.log(true)
-      //       data.comments.sort(function(a, b){
-      //         return b[attribute]-a[attribute]
-      //       })
-      //     case "關注":
-      //       console.log(true)
-      //       var temp = data.comments.sort(function(a, b){
-      //         return b.followers.length-a.followers.length
-      //       })
-      //       console.log(temp)
-      //   }
-      // }
-    }
-    var comments;
+    console.log("comment data: ", data)
+    console.log("sort: ", sort)
+    
+    var comments = [];
     if (!loading) {
-      console.log("comment data: ", data)
-      comments = data.comments;
+      comments = JSON.parse(JSON.stringify(data.comments));
+      console.log("comments: ", comments);
+      switch(sort) {
+        case "最新":
+          comments.reverse();
+          break;
+        case "關注":
+          comments.sort((a, b) => b.followers.length-a.followers.length);
+          break;
+        case "回覆":
+          comments.sort((a, b) => b.replies.length-a.replies.length);
+          break;
+        default:
+          break;
+      }
     }
-    console.log("comments: ", comments);
-    switch(sort) {
-      case "最新":
-        // sort comments;
-        break;
-      case "關注":
-        // sort comments;
-        break;
-      case "回覆":
-        // sort comments;
-        break;
-      default:
-        break;
-    }
-
+  
     useEffect(() => {
         try {
             subscribeToMore({
