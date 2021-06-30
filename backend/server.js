38a9ac2d9c96d24aecba37c8280e820ecfce39ea
PJL-Emily <b07705053@ntu.edu.tw>
2021-06-30 17:28:13 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -28,9 +28,9 @@ const pubsub = new PubSub();
 const app = express();
 
 app.use(cors());
-app.use(express.static(path.join(__dirname, "build")));
+app.use(express.static(path.join(__dirname, "../frontend/build")));
 app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+  res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
 });
 
 const server = new ApolloServer({
